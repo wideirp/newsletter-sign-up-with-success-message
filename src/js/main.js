46b1b875707b65e19e0b9a0d5d949e6d7101629d
wideirp <51214984+wideirp@users.noreply.github.com>
2024-08-20 17:24:11 -0500

    Add email validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,62 @@
 const signup = document.querySelector(".sign-up");
 const success = document.querySelector(".success");
 const form = document.getElementById("form");
+const emailInput = form.querySelector("#email");
 const userEmailDisplay = document.getElementById("user-email");
+const emailError = document.getElementById("email-error");
 const successBtn = document.getElementById("success-btn");
+
+const [VISIBLE, HIDDEN] = ["visible", "hidden"];
+
+const validateEmail = (email) => {
+  const emailPattern = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
+  const isValid = emailPattern.test(email);
+  return isValid;
+};
+
+const showInputError = () => {
+  emailInput.dataset.error = VISIBLE;
+  emailError.dataset.error = VISIBLE;
+};
+
+const hideInputError = () => {
+  emailInput.dataset.error = HIDDEN;
+  emailError.dataset.error = HIDDEN;
+};
+
+emailInput.addEventListener("input", (event) => {
+  event.preventDefault();
+  const emailEl = event.target;
+  console.log(emailEl);
+  if (emailEl.hasAttribute("edited")) {
+    const isValid = validateEmail(emailEl.value);
+    console.log(emailEl.value, isValid);
+    if (isValid) {
+      hideInputError();
+    } else {
+      showInputError();
+    }
+  }
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const enteredEmail = event.target.querySelector("#email").value;
-  event.target.querySelector("#email").value = "";
-  userEmailDisplay.textContent = enteredEmail;
-  signup.dataset.state = "hidden";
-  success.dataset.state = "visible";
+  const emailEl = event.target.querySelector("#email");
+  let email = emailEl.value;
+  const isValid = validateEmail(email);
+  emailEl.setAttribute("edited", "");
+
+  if (isValid) {
+    emailEl.value = "";
+    userEmailDisplay.textContent = email;
+    signup.dataset.state = HIDDEN;
+    success.dataset.state = VISIBLE;
+  } else {
+    showInputError();
+  }
 });
 
 successBtn.addEventListener("click", () => {
-  signup.dataset.state = "visible";
-  success.dataset.state = "hidden";
+  signup.dataset.state = VISIBLE;
+  success.dataset.state = HIDDEN;
 });
